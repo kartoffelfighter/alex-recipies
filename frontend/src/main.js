f0869c53817c738fd7e1523ccbe9dc4ab8610fc4
Marc Fischer <marc@marcfischer.org>
2019-08-15 00:14:32 +0200

    removed global api

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -6,12 +6,12 @@ import 'roboto-fontface/css/roboto/roboto-fontface.css'
 import 'material-design-icons-iconfont/dist/material-design-icons.css'
 
 import router from './router'
-import store from './store'
-import axios from './Api'
+import store from './store/'
+
 
 import VueEditor from "vue2-editor/";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = true
 
 Vue.use(VueEditor);
 
@@ -19,6 +19,6 @@ new Vue({
   router,
   store,
   VueEditor,
-  axios,
+
   render: h => h(App)
 }).$mount('#app')
