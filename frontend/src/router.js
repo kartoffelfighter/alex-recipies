92e4451ae5511e5d867bd8035155ebcc9dd5e14a
Marc Fischer <marc@marcfischer.org>
2019-08-15 00:15:03 +0200

    added auth to routes

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -6,24 +6,49 @@ import loggedOutIndex from '@/components/logged-out/index.vue'
 import dashboard from '@/components/logged-in/dashboard/index.vue'
 
 import recipe from '@/components/logged-in/recipe/recipe.vue'
+import store from './store';
 Vue.use(Router)
 
-export default new Router({
+const router = new Router({
   routes: [
     {
       path: '/',
       name: 'home',
-      component: loggedOutIndex
+      component: loggedOutIndex,
+      meta: {
+        auth: false,
+      }
     },
     {
       path: '/dashboard',
       name: 'dashboard',
-      component: dashboard
+      component: dashboard,
+      meta: {
+        auth: true,
+      }
     },
     {
       path: '/recipe/:id',
       name: 'recipe',
-      component: recipe
+      component: recipe,
+      meta: {
+        auth: true,
+      }
     }
   ]
 })
+
+
+router.beforeEach((to, from, next) => {
+  if (to.matched.some(record => record.meta.auth)) {
+    if (store.getters.ISLOGGEDIN) {
+      next()
+      return
+    }
+    next('/')
+  } else {
+    next()
+  }
+})
+
+export default router
