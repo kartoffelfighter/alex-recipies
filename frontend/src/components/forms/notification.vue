3d83aca3bb6e54f2f1150a310629c73ae1fe20e3
Marc Fischer <marc@marcfischer.org>
2019-08-15 00:15:41 +0200

    implemented notification view

---
+++ frontend/src/components/forms/notification.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-snackbar v-model="visible" color="green lighten-2" top>
+    {{message}}
+    <v-btn color="pink" flat @click="dismiss()">Close</v-btn>
+  </v-snackbar>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    visible: false,
+    message: null
+  }),
+  methods: {
+    dismiss() {
+      this.$store.dispatch("DISMISS_NOTIFY");
+    }
+  },
+  created: function() {
+    this.$store.watch(
+      state => state.notify,
+      () => {
+        this.message = this.$store.getters.NOTIFYMSG;
+        this.visible = true;
+      }
+    );
+  }
+};
+</script>
