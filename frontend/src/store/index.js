5da27267cda7ccd7be780005adaf65e77d22bcaf
Marc Fischer <marc@marcfischer.org>
2019-08-15 00:15:18 +0200

    new storage handling

---
+++ frontend/src/store/index.js
@@ -0,0 +1,50 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+import recipes from './modules/recipes'
+import recipe from './modules/recipe'
+import user from './modules/user'
+
+
+Vue.use(Vuex);
+
+const store = new Vuex.Store({
+  state: {
+    loading: false,
+    notify: false,
+    notifyMSG: null,
+  },
+  getters: {
+    LOADING: state => {
+      return state.loading
+    },
+    NOTIFY: state => {
+      return state.notify
+    },
+    NOTIFYMSG: state => {
+      return state.notifyMSG
+    }
+  },
+  mutations: {
+    LOAD: (state) => {
+      state.loading = !state.loading;
+    },
+    DO_NOTIFY: (state, msg) => {
+      state.notify = true,
+      state.notifyMSG = msg
+    },
+    DISMISS_NOTIFY: (state) => {
+      state.notify = false
+    }
+  },
+  actions: {},
+
+  modules: {
+    recipe,
+    recipes,
+    user
+  }
+
+})
+
+export default store
---
+++ frontend/src/store/modules/user.js
@@ -0,0 +1,135 @@
+import Axios from 'axios'
+
+const API = 'http://localhost/api/v1/account/';
+const header = {
+    'cache-control': 'no-cache',
+    Connection: 'keep-alive',
+    'content-length': '51',
+    'accept-encoding': 'gzip, deflate',
+    Host: 'localhost',
+    'Postman-Token': '620ecb30-3f15-4de6-8ac7-b9ddaa99bc46,a960a43f-45d3-4890-985b-2948b08a99fd',
+    'Cache-Control': 'no-cache',
+    Accept: '*/*',
+    'User-Agent': 'PostmanRuntime/7.15.0',
+    'Content-Type': 'text/plain'
+};
+
+const user = {
+    //namespaced: true,
+    state: {
+        id: null,
+        email: null,
+        token: null,
+        name: null,
+        tokenLifeTime: null,
+        status: null,
+        loggedIn: false,
+    },
+
+    getters: {
+        TOKEN: state => {
+            return state.token;
+        },
+        EMAIL: state => {
+            return state.email;
+        },
+        NAME: state => {
+            return state.name;
+        },
+        TOKENLIFETIME: state => {
+            return state.tokenLifeTime;
+        },
+        STATUS: state => {
+            return state.status;
+        },
+        ISLOGGEDIN: state => {
+            return state.loggedIn
+        }
+    },
+
+    mutations: {
+        SET_TOKEN: (state, payload) => {
+            state.token = payload;
+        },
+        SET_EMAIL: (state, payload) => {
+            state.email = payload;
+        },
+        SET_ID: (state, payload) => {
+            state.id = payload;
+        },
+        SET_NAME: (state, payload) => {
+            state.name = payload;
+        },
+        SET_TOKENLIFETIME: (state, payload) => {
+            state.tokenLifeTime = payload;
+        },
+        LOGING_IN: (state, ) => {
+            state.status = "LOADING...";
+        },
+        LOGGED_IN: (state, ) => {
+            state.status = "LOGGED IN!";
+            state.loggedIn = true;
+        },
+        LOGGED_OUT: (state, ) => {
+            state.status = "LOGGED OUT!";
+            state.loggedIn = false;
+        },
+        LOGIN_ERR: (state, status) => {
+            state.status = status;
+            state.loggedIn = false;
+        }
+    },
+
+    // see https://github.com/kartoffelfighter/alex-recipies/blob/master/doku/ for api description
+    actions: {
+        LOG_IN({ commit }, data) {
+            return new Promise((resolve, reject) => {
+                commit('LOAD')
+                Axios({
+                    url: API + 'login.php',
+                    method: 'POST',
+                    header: header,
+                    data: JSON.stringify(data)
+                })
+                    .then(resp => {
+                        const token = resp.data.token
+                        const lifetime = resp.data.lifetime
+                        const id = resp.data.id
+                        commit('SET_TOKEN', token)
+                        commit('SET_TOKENLIFETIME', lifetime)
+                        commit('SET_ID', id)
+                        commit('LOGGED_IN')
+                        commit('LOAD')
+                        commit('DO_NOTIFY', "Login erfolgreich!")
+                        resolve(resp)
+                    })
+                    .catch(err => {
+                        commit('LOGIN_ERR', err)
+                        commit('LOAD')
+                        reject(err)
+                    })
+            })
+        },
+        REGISTER({ commit }, data) {
+            return new Promise((resolve, reject) => {
+                commit('LOAD')
+                Axios({
+                    url: API + 'create.php',
+                    method: 'POST',
+                    header: header,
+                    data: JSON.stringify(data)
+                })
+                    .then(resp => {
+                        commit('DO_NOTIFY', "Benutzer wurde angelegt!")
+                        resolve(resp)
+                    })
+                    .catch(err => {
+                        commit('DO_NOTIFY', "Fehler beim anlegen des Benutzers!")
+                        reject(err)
+                    })
+            })
+        }
+    }
+}
+
+export default user
