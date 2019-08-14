bc9644c9d5e8c51aeac29291e5e65f21cdc4710b
Marc Fischer <marc@marcfischer.org>
2019-08-15 00:15:29 +0200

    implemented login function

--- frontend/src/components/forms/login.vue
+++ frontend/src/components/forms/login.vue
@@ -2,29 +2,22 @@
   <v-container text-xs-centered>
     <v-layout row wrap>
       <v-flex min-width="500px">
-        <v-form ref="form" v-model="valid" lazy-validation>
+        <v-form ref="form" v-model="valid" lazy-validation @submit.prevent="login()">
           <v-text-field v-model="email" :rules="emailRules" label="E-mail" required></v-text-field>
-          <v-text-field v-model="password" label="Passwort" value :rules="passwordRules" type="password"></v-text-field>
+          <v-text-field v-model="password" label="Passwort" :rules="passwordRules" type="password"></v-text-field>
           <v-divider></v-divider>
 
-          <v-btn :disabled="!valid" color="success" @click="validate">Anmelden</v-btn>
+          <v-btn :disabled="!valid" color="success" type="submit">Anmelden</v-btn>
           <v-btn color="warning" disabled>Passwort vergessen</v-btn>
-          <v-btn color="warning"  @click="storeTest()">Passwort vergessen</v-btn>
         </v-form>
       </v-flex>
     </v-layout>
-    <v-snackbar
-      v-model="snackbar"
-      color="green lighten-2"
-      top
-    >
-      Login erfolgreich
-      <v-btn color="pink" flat @click="snackbar = false">Close</v-btn>
-    </v-snackbar>
+
   </v-container>
 </template>
 
 <script>
+
 export default {
   data: () => ({
     valid: false,
@@ -35,8 +28,7 @@ export default {
       v => !!v || "Du musst eine Email angeben",
       v => /.+@.+/.test(v) || "Das sieht aber nicht nach einer Email aus"
     ],
-    checkbox: true,
-    snackbar: false
+    checkbox: true,  
   }),
 
   methods: {
@@ -53,12 +45,19 @@ export default {
     },
     storeTest(){
      // console.log(this.currentUser);
+    },
+    login: function() {
+      this.$store.dispatch('LOG_IN', {email: this.email, password: this.password })
+      .then(() => this.$router.push('/dashboard'))
     }
   },
   computed: {
-    currentUser(){
-      return this.$store.state.currentUser['jwt'];
-    }
+   status() {
+     return this.$store.getters.STATUS
+   },
+   loading() {
+     return this.$store.getters.LOADING
+   }
   }
 };
 </script>
